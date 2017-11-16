0c5178eb0c6266c3134ad020c82dd01a338a7cbd
Remi Plantade <d0r3mu5@gmail.com>
2017-11-16 13:39:40 +0100

    Generate Bean from database and call ant job from java with maven

---
+++ src/api_builder/app/Runner.java
@@ -0,0 +1,32 @@
+package api_builder.app;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		InvocationRequest request = new DefaultInvocationRequest();
+		request.setPomFile( new File( "pom.xml" ) );
+		request.setGoals( Arrays.asList( "clean", "install"," antrun:run@hbm2java" ) );
+		
+		Invoker invoker = new DefaultInvoker();
+		invoker.setMavenExecutable(new File("mvn.cmd"));
+		invoker.setMavenHome(new File("maven"));
+		try {
+			invoker.execute( request );
+		} catch (MavenInvocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
