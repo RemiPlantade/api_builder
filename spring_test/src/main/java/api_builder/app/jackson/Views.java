f3d481df1c986056bfe67f4446df7504000cfcfb
remi_ <d0r3mu5@gmail.com>
2017-12-12 15:16:11 +0100

    Costomizing Jackson Serialisation

--- spring_test/src/main/java/api_builder/app/jackson/Views.java
+++ spring_test/src/main/java/api_builder/app/jackson/Views.java
@@ -1,6 +1,12 @@
 package api_builder.app.jackson;
 
 public class Views {
-	 public static class Public {
-	  }
+	public static class Public {
+	}
+	public static class VoitureView {
+	}
+	public static class ConducteurView {
+	}
+	public static class RoueView {
+	}
 }
