69e8213b332091dc6adb4c8443610cef6b6f696a
Remi Plantade <d0r3mu5@gmail.com>
2018-02-22 23:04:31 +0100

    sWITCH TO tHYMElEAF

--- spring_test/src/main/java/api_builder/app/WebMvcConfiguration.java
+++ spring_test/src/main/java/api_builder/app/WebMvcConfiguration.java
@@ -14,7 +14,7 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		 registry.addViewController("/login").setViewName("/templates/login");
+		 registry.addViewController("/login.ftl").setViewName("/login");
 	     registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
 	}
