b5038135ed7f7257b3e3a8301b2b9e06ed2fcf82
remi_ <d0r3mu5@gmail.com>
2018-03-02 19:10:52 +0100

    IMplement restart

--- spring_test/src/main/java/api_builder/app/conf/controller/ApiConfController.java
+++ spring_test/src/main/java/api_builder/app/conf/controller/ApiConfController.java
@@ -1,30 +1,55 @@
 package api_builder.app.conf.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import api_builder.app.conf.model.form.ApiConfWrapper;
+import api_builder.app.conf.service.ApiConfService;
 
 @Controller
+@SessionAttributes("apiConfWrapper")
+//@RefreshScope
 public class ApiConfController {
-
-	@GetMapping("/")
-	public String displayIndex() {
-		return "index";
+	
+//	@Value("${server.port}")
+//	private String value;
+	
+	
+	@Autowired
+	private ApiConfService apiConfService;	
+	
+	@ModelAttribute("apiConfWrapper")
+	public ApiConfWrapper addNewRapperConf() {
+		return getApiConfWrapper();
 	}
 
-	@GetMapping("/login")
-	public String displayLogin() {
-		return "login";
+	@GetMapping("/admin/general")
+	public String displayUserConf(Model model) {
+		model.addAttribute("apiConfWrapper", getApiConfWrapper());
+		return "admin/general";
 	}
-	// Login form with error
-	@GetMapping("/login-error")
-	public String loginError(Model model) {
-		model.addAttribute("loginError", true);
-		return "login";
-	}
+	
 
-	@GetMapping("/admin")
-	public String displayAdminHome() {
-		return "admin/admin";
+	@PostMapping(value = "/admin/general")
+	public String addUser(HttpSession session, @ModelAttribute("apiConfWrapper") ApiConfWrapper apiConfWrapper, Model model, SessionStatus sa) {
+		apiConfService.updateConfFromWrapper(apiConfWrapper);
+		session.removeAttribute("apiConfWrapper");
+		//value = apiConfService.findByParamName("Port").getParamValue();
+		sa.setComplete();
+		return "admin/general";
+	}
+	
+	public ApiConfWrapper getApiConfWrapper() {
+		ApiConfWrapper apiconfWrapper = new ApiConfWrapper();
+		apiconfWrapper.setApiConfList(apiConfService.findAll());
+		return apiconfWrapper;
 	}
 }
