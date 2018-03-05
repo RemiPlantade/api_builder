c397312b5e2a8d2ebbe2639ce0b2fff7c5992c9b
Remi Plantade <d0r3mu5@gmail.com>
2018-03-05 22:27:12 +0100

    Enable HTTPS redirection

--- spring_test/src/main/java/api_builder/app/CustomPropertySource.java
+++
@@ -1,8 +0,0 @@
-package api_builder.app;
-
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-
-public class CustomPropertySource extends PropertyPlaceholderConfigurer {
-
-	
-}
